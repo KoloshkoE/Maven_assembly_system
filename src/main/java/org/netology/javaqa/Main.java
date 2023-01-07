c57cdaab8e1e490788ec3579ad9c168ad10e2c8a
Koloshko Evgeni <kolohkoe@gmail.com>
2023-01-07 21:41:00 +0700

    initial commit

---
+++ src/main/java/org/netology/javaqa/Main.java
@@ -0,0 +1,11 @@
+package org.netology.javaqa;
+
+public class Main {
+    public static void main(String[] args) {
+       // BonusService asd = new BonusService();
+        //long amount = 1000;
+        //boolean registered = true;
+        //long expected = 30;
+        System.out.println("Hello world!");
+    }
+}
